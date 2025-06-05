e32efd396ed726b2b237ec7c176086f34fb491e8
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-06-05 10:19:15 +0530

    05/06

---
+++ 12_comment.py
@@ -0,0 +1,12 @@
+#this is single line comment
+#this is also single line comment
+'this is single line comment by sting literal'
+'''hi this is 
+multiline comment
+using string literals'''
+
+"""hi this is also 
+multiline comment
+using string literals"""
+name="chetan"
+print(f"name of student : {name}")
---
+++ 13_airthmatic.py
@@ -0,0 +1,16 @@
+#example of airthmatic operator
+a=eval(input("enter first num : ")) #5
+b=eval(input("enter second num : "))# 3
+ans=a+b
+print("addition : ",ans)#8
+ans=a-b
+print("subtraction : ",ans)#2
+ans=a*b
+print("multiplication : ",ans)#15
+ans=a/b
+print("division : ",ans)#1.6
+ans=a//b
+print("floor division : ",ans)#1
+ans=a%b
+print("modules  : ",ans)#2
+print("power  : ",a**b)#125
