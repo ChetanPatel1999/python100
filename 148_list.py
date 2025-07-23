fce022037eee8fec06b318f6723d532cf3a9bcdf
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-23 10:43:39 +0530

    22/07

---
+++ 148_list.py
@@ -0,0 +1,17 @@
+# The list() Constructor
+#  It is also possible to use the list() constructor when creating a new list. 
+
+marks=[12,45,67,89,34]
+
+fruits=list(("orange","mango","apple","banana"))
+l1=list("home")
+l2=list({23,45,67,89})
+l3=list({"pen":10,"book":50})
+l4=list((78,89,100))
+print(l4)
+print(l1)
+print(l2)
+print(l3)
+print(marks)
+print(fruits)
+print(type(fruits))
